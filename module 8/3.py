5569b2be6909c4810907fb2c3cd3c7b1aad71196
Md. Mohabbat Ali <129573768+md-mohabbat-ezekiel@users.noreply.github.com>
2023-11-01 21:00:55 +0600

    Update 3.py

--- module 8/3.py
+++ module 8/3.py
@@ -1,43 +1,29 @@
 class Star_Cinema:
     hall_list = []
-
-    @classmethod
-    def add_hall(cls, hall_object):
-        if isinstance(hall_object, Hall):
-            cls.hall_list.append(hall_object)
-            print(f"Hall '{hall_object.hall_no}' added to the hall list.")
+    def entry_hall(self, hall):
+        if isinstance(hall, Hall):
+            self.hall_list.append(hall)
         else:
-            print("Invalid input. Please provide an object of class 'Hall'.")
+            print("Invalid object. Only objects of class Hall can be added to hall_list.")
 
-class Hall(Star_Cinema):
+class Hall:
     def __init__(self, rows, cols, hall_no):
         self.rows = rows
         self.cols = cols
         self.hall_no = hall_no
-        self.seats = {} 
+        self.seats = {}
         self.show_list = []
-        self.create_seats() 
-        self.entry_hall() 
+        self.seats = {f'id_{hall_no}': [['Free' for _ in range(cols)] for _ in range(rows)}
+        Star_Cinema.hall_list.append(self)
 
-    def create_seats(self):
-        self.seats = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
-
-    def entry_hall(self):
-        self.add_hall(self)
-
-    def entry_show(self, show_id, movie_name, time):
-        show_info = (show_id, movie_name, time)
+    def entry_show(self, id, movie_name, time):
+        show_info = (id, movie_name, time)
         self.show_list.append(show_info)
-        self.seats[show_id] = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
-
-hall1 = Hall(rows=5, cols=10, hall_no=1)
-
-hall1.entry_show(show_id=1, movie_name="Movie 1", time="10:00 AM")
-
-print("Show List:")
+cinema = Star_Cinema()
+hall1 = Hall(5, 10, 1)
+hall1.entry_show("show1", "Movie 1", "8:00 PM")
+print("Show List in Hall 1:")
 for show in hall1.show_list:
     print(f"Show ID: {show[0]}, Movie: {show[1]}, Time: {show[2]}")
-
-print("\nSeats for Show ID 1:")
-for row in hall1.seats[1]:
-    print(row)
+print("Seats in Hall 1:")
+print(hall1.seats)
